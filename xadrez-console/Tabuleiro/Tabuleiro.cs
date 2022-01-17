59b0b325eb6f944b8b1337bae548509284ea8000
Argeu Bettoni <argeu.bettoni@gmail.com>
2022-01-17 14:50:08 -0300

    Comecando a implementar o tabuleiro

---
+++ xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+
+        public Peca[,] pesas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            this.linhas = linhas;
+            this.colunas = colunas;
+            this.pesas = new Peca[linhas, colunas];
+        }
+    }
+}
